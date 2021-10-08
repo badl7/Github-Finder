0e9311031615019c3bfbcd07cd3e19ca2c6fbf00
badl7 <btlgrbz071@gmail.com>
2021-10-08 17:28:40 +0300

    first

--- github.py
+++ github.py
@@ -8,9 +8,18 @@ def index():
     if request.method=='POST':
         # Handle POST Request here
         githubname = request.form.get("githubname")
-        response = requests.get(base_url + githubname)
-        user_info = response.json()
-        return render_template('index.html', profile = user_info)
+        response_user = requests.get(base_url + githubname)
+        response_repos = requests.get(base_url + githubname + "/repos")
+
+        user_info = response_user.json()
+        repos = response_repos.json()
+
+
+        if "message" in user_info:
+            return render_template('index.html', error = "Kullanıcı Bulunamadı...")
+        else:
+            return render_template('index.html', profile = user_info , repos = repos)
+
     else:
         return render_template('index.html')
 
